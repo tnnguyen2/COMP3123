25dce92c591728c0f56bdb5902a259f6b2449e39
tnnguyen2 <tony.nguyen2@georgebrown.ca>
2023-09-30 14:11:16 -0400

    COMP3123 2023-09-30 LAB EXERCISE

---
+++ Week04_Fri_Js/100907949_COMP3123-exec04/100907949_COMP3123-exec04.js
@@ -0,0 +1,32 @@
+let express = require('express')
+
+const SERVER_PORT = 8089
+let app = express()
+app.use(express.json())
+
+//http://localhost:8089/hello
+app.get("/hello",(req,res)=>{
+    res.send("Hello Express JS")
+})
+
+//QUERY Parameter
+//http://localhost:8089/user?firstname=Tony&lastname=Nguyen
+app.get("/user",(req,res)=>{
+    const data = req.query;
+        res.send(data)
+})
+
+//PATH Parameter
+//http://localhost:8089/user/tony/nguyen
+app.post("/user/:firstname/:lastname",(req,res)=>{
+    const data = req.params
+    let firstname = req.params.firstname
+    let lastname = req.params.lastname
+
+    res.send(data)
+
+})
+
+app.listen(SERVER_PORT,()=>{
+    console.log(`Server is running at http://localhost:${SERVER_PORT}`)
+})
